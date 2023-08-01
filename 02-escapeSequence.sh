bdd42cc98dd4089db4226d2ef33a3bd12da1c719
Lokesh Karasala <lokeshkarasala@Lokeshs-iMac.local>
2023-08-01 23:46:47 +0530

    added to command for new line and tab space

---
+++ 02-escapeSequence.sh
@@ -0,0 +1,24 @@
+#shebang notation
+
+echo welcome to bash scripting
+
+#This is how we execute the scripts
+ $ bash scriptname.sh or ah scriptName.sh or ./scriptName.sh
+
+
+# \n : indicates new line
+example: Manoj\ndevops
+,,,,,Manoj,,,,
+'''''Devops'''
+
+echo Line1
+Echo line2
+echo line3
+
+echo -e "Line4\nLine5"
+# whev ever we are using special characters, we use double quotes:" "
+
+
+# \t : This gives a tab space
+example: echo -e "cloud\tdevops\tis\tbooming
+
