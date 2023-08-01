a82bf09cd6540d899866384665ee026b5b3e0d45
Lokesh Karasala <lokeshkarasala@Lokeshs-iMac.local>
2023-08-02 00:27:47 +0530

    learning colour commans

---
+++ 03-colours.sh
@@ -0,0 +1,18 @@
+#!bin/bash
+
+# Each and every color you see on terminal will have a color code and we need to use that code baesd on our need.
+
+# Colors       Foreground          Background
+# Red               31                  41
+# Green             32                  42
+# Yellow            33                  43
+# Blue              34                  44
+# Magenta           35                  45
+# Cyan              36                  46
+
+# Syntax To Print Color Text Is :
+# Ex : 
+#echo -e "\e[COLOURCODEm Your mes to be printed in colour \e[0m"
+
+echo -e "\e31m Iam learning bash scripting \e[0m"
+echo -e "\e36m Iam learining bash scripting \e[0m"
