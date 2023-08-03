906aebeef7046f170d91afa960d168cae5d3527f
Lokesh Karasala <lokeshkarasala@Lokeshs-iMac.local>
2023-08-03 23:48:00 +0530

    added comments

--- 06-specialVariables.sh
+++ 06-specialVariables.sh
@@ -18,5 +18,10 @@ echo "Executed script name is : $0"
 
 echo "iam learning $1"
 echo "we need to learn $2"
-echo "its a 45days course to learn $3
+echo "its a 45days course to learn $3"
 
+echo $$    # $$ is going to print the PID of the current proces 
+echo $#    # $# is going to print the number of arguments
+echo $?    # $? is going to print the exit code of the last command.
+echo $*    # $* is going to print the used arguments
+echo $@    # $* is going to print the used arguments
