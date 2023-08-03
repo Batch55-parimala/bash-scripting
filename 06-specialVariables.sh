95f7870ff57b3463b75e0f95e6287d84726c31fb
Lokesh Karasala <lokeshkarasala@Lokeshs-iMac.local>
2023-08-03 23:36:09 +0530

    updated the comments

--- 06-specialVariables.sh
+++ 06-specialVariables.sh
@@ -14,23 +14,9 @@ echo value of b is $b
 echo value of c is $c
 
 echo $0
-echo "Executed script name is : $0
+echo "Executed script name is : $0"
 
-echo "Iam learning $1"
-echo "to learn devops it needs $2"
-echo "iam getting trained on $3"
+echo "iam learning $1"
+echo "we need to learn $2"
+echo "its a 45days course to learn $3
 
-
-#  bash scriptName.sh  arg1  arg2  arg3
-# bash arg1  arg2  arg3 arg4  arg5  arg6 arg7  arg8  arg9  arg10 
-#        1     2     3    4    5     6    7      8    9      10
-
-# bash scriptName.sh 100   200   300    ( like this you can supply a maximum of 9 variables from the command line)
-#                     $1    $2    $3  
-
-
-echo $$    # $$ is going to print the PID of the current proces 
-echo $#    # $# is going to print the number of arguments
-echo $?    # $? is going to print the exit code of the last command.
-echo $*    # $* is going to print the used arguments
-echo $@    # $* is going to print the used arguments
