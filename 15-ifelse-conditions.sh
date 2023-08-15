402d294818633307a139a03e841d0aa7f14bff79
Lokesh Karasala <lokeshkarasala@Lokeshs-iMac.local>
2023-08-15 18:47:18 +0530

    updating file

--- 15-ifelse-conditions.sh
+++ 15-ifelse-conditions.sh
@@ -4,7 +4,7 @@ echo "demo on if and of-else and else-if usage"
 
 ACTION=$1
 
-if ["$ACTION" = "Start"]; then
+if [ "$ACTION" == "Start" ]; then
    echo -e "\e[32m starting payment service \e[0m"
    exit 0
 fi
