016e4e50c77cf780b2c6acc74a489d3929daa185
Lokesh Karasala <lokeshkarasala@Lokeshs-iMac.local>
2023-08-15 13:14:54 +0530

    updated the file

--- 12-conditions.sh
+++ 12-conditions.sh
@@ -12,7 +12,7 @@ Action=$1
 
 case $Action in
    start)
-       echo -e "\e[32m starting payment service \e{0m"
+       echo -e "\e[32m starting payment service \e[0m"
        exit 0
        ;;
    stop)
@@ -24,8 +24,8 @@ case $Action in
        exit 2
        ;;
     *)
-       echo -e "\e[35m Valid options are strt or srestart \e[0m"
-       echo -e "\e[33m Example usage \e[0m : /n /t bash scriptname stop"
+       echo -e "\e[35m Valid options are start or stop or restart \e[0m"
+       echo -e "\e[33m Example usage \e[0m : \n \t bash scriptname stop"
        exit 3
        ;;
 esac       
