1a3043a1ff8cd0fa15998952e197b545c1e78f44
Lokesh Karasala <lokeshkarasala@Lokeshs-iMac.local>
2023-08-15 13:02:27 +0530

    updated case funtion

---
+++ 12-conditions.sh
@@ -0,0 +1,32 @@
+#!bin/bash
+
+# Conditions helps us to execute something opnly if some factors is true and if that factor is false it will nolt execute.
+
+# Syntax of CASE
+# case $var in
+#   opt1) command-x ;;
+#   opt2) command-y ;;
+# esac
+  
+Action=$1
+
+case $Action in
+   strat)
+       echo -e "\e[32m starting payment service \e{0m"
+       exit 0
+       ;;
+   stop)
+       echo -e "\e[31 m stopping the payment service \e[0m"
+       exit 1
+       ;;
+   restart)
+       echo -e "\e[33m Restarting the payment serice \e[0m"
+       exit 2
+       ;;
+   *)
+       echo -e "\e[35m Valid Options are start or stop or restart \e[0m"
+       echo -e "\e[33m Example Usage \e[0m :\n \t bash scriptName stop"
+       exit 3
+        ;;
+
+ esac       
