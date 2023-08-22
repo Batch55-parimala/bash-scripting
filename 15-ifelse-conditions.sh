240f5dd41df6b5f7536327533156fbf1bd8560ba
Lokesh Karasala <lokeshkarasala@Lokeshs-iMac.local>
2023-08-22 11:19:53 +0530

    if else

--- 15-ifelse-conditions.sh
+++ 15-ifelse-conditions.sh
@@ -5,6 +5,19 @@ echo "demo on if and of-else and else-if usage"
 ACTION=$1
 
 if [ "$ACTION" == "Start" ]; then
-   echo -e "starting payment"
+   echo -e "\e[32m starting payment \e[0m"
    exit 0
+
+ elif [ "$ACTION" == "stop" ]; then
+      echo -e "\e[33m stopping payment \e[0m"
+      exit1
+
+elif [ "$ACTION" == "restart" ]; then
+      echo -e "\e[35m restarting payment \e[0m"
+      exit2 
+
+ else
+       echo -e "\e[35m Valid options are start or stop or restart \e[0m"
+       echo -e "\e[33m Example usage \e[0m : \n \t bash scriptname stop"
+       exit 3          
 fi
