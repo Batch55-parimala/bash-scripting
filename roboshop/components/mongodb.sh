abcddf15612a2b7709803a32cf1cd6f78e4f8df3
Lokesh Karasala <lokeshkarasala@Lokeshs-iMac.local>
2023-09-04 13:00:31 +0530

    updated

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -46,7 +46,7 @@ stat $?
 
 echo -n "Extracting ${COMPONENT} schema:"
 cd /tmp
-unzip ${COMPONENT}.zip  &>>  ${LOGFILE}
+unzip -o ${COMPONENT}.zip  &>>  ${LOGFILE}
 stat $?
 
 echo -n "Injecting ${COMPONENT} schema:"
