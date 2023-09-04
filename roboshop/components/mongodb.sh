f27a00ef7f79cd5f2caa958bf0dd3a95e16e8a46
Lokesh Karasala <lokeshkarasala@Lokeshs-iMac.local>
2023-09-04 12:59:17 +0530

    updated

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -17,6 +17,7 @@ stat() {
        echo -e "\e[32m success \e[0m"
     else  
        echo -e "\e[31m failure \e[0m"
+       exit 2
     fi
 }   
 
@@ -31,10 +32,27 @@ yum install -y mongodb-org    &>>  ${LOGFILE}
 stat $?
 
 echo -n "Enabling  ${COMPONENT} visibility:"
-sed  -ie 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+sed  -ie 's/127.0.0.1/0.0.0.0/g' mongod.conf
 stat $?
 
 echo -n "Starting  ${COMPONENT} service:"
 systemctl enable mongod    &>>  ${LOGFILE}
 systemctl start mongod     &>>  ${LOGFILE}
 stat $?
+
+echo -n "Downloading the ${COMPONENT} schema:"
+curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip"
+stat $?
+
+echo -n "Extracting ${COMPONENT} schema:"
+cd /tmp
+unzip ${COMPONENT}.zip  &>>  ${LOGFILE}
+stat $?
+
+echo -n "Injecting ${COMPONENT} schema:"
+cd ${COMPONENT}-main
+mongo < catalogue.js    &>>  ${LOGFILE}
+mongo < users.js        &>>  ${LOGFILE}
+stat $?
+
+echo -e "\e[35m Installation completed ${COMPONENT} \e[0m \n"
