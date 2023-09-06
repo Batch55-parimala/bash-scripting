e2a992b2a9c44335e24884d32f955734f041a049
Lokesh Karasala <lokeshkarasala@Lokeshs-iMac.local>
2023-09-06 19:00:15 +0530

    updated

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -61,7 +61,7 @@ stat $?
 
 echo -n "Updating the ${COMPONENT} service :"
 sed -ie 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/${APPUSER}/${COMPONENT}/systemd.service
-mv /home/${APPUSER}/${COMPONENT}/systemd.servicee /etc/systemd/system/${COMPONENT}.service
+mv /home/${APPUSER}/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service
 stat $?
 
 echo -n "Starting the  ${COMPONENT} service:"
