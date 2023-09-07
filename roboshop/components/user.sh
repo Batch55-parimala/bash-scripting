bfee1e4be4d47f9378280f29e626941ab37fa205
Lokesh Karasala <lokeshkarasala@Lokeshs-iMac.local>
2023-09-07 19:10:31 +0530

    user

--- roboshop/components/user.sh
+++ roboshop/components/user.sh
@@ -60,7 +60,7 @@ npm install  &>>  ${LOGFILE}
 stat $?
 
 echo -n "Updating the ${COMPONENT} service :"
-sed -ie 's/REDIS_DNSNAME/redis.roboshop.internal/' -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/${APPUSER}/${COMPONENT}/systemd.service
+sed -ie 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' /home/${APPUSER}/${COMPONENT}/systemd.service
 mv /home/${APPUSER}/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service
 stat $?
 
