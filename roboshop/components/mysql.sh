f56cd641eb72a8317da73617614c19025bf3398a
Lokesh Karasala <lokeshkarasala@Lokeshs-iMac.local>
2023-11-04 00:10:49 +0530

    up

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -28,7 +28,7 @@ stat $?
 echo "show databases;" | mysql -uroot -pRoboShop@1    &>>  ${LOGFILE}
 if [ $? -ne 0 ];then
 echo -n "Performing default password reset of root account :"
-echo "ALTER USER 'root'@'localhost' INDENTIFIED by 'RoboShop@1'" | mysql --connect-expired-password -uroot -p$DEFAULT_ROOT_PASSWORD    &>>  ${LOGFILE}
+echo "ALTER USER 'root'@'localhost' INDENTIFIED by 'RoboShop@1'" | mysql  --connect-expired-password -uroot -p$DEFAULT_ROOT_PASSWORD     &>>  ${LOGFILE}
 stat $?
 fi
 
