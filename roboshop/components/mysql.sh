f2526c04d4f887e5bbd0ebfcb3eda611f219f498
Lokesh Karasala <lokeshkarasala@Lokeshs-iMac.local>
2023-11-03 11:54:57 +0530

    mysql

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -1,3 +1,16 @@
 #!bin/bash
 
-echo "iam mysql"
+COMPONENT=mysql
+
+source components/common.sh
+
+
+echo -e "\e[35m configuring ${COMPONENT}......! \e[0m \n"
+
+echo -n "\e[35m Configuring ${COMPONENT} repo \e[0m \n"
+curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/stans-robot-project/mysql/main/mysql.repo
+stat $?
+
+echo -n "Installing NodeJS :"
+yum install mysql-community-server -y   &>>  ${LOGFILE}
+stat $?
