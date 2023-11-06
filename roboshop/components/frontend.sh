a338122edd4c771eb4321c8ffe0cb20bb3ccd00a
Lokesh Karasala <lokeshkarasala@Lokeshs-iMac.local>
2023-11-06 14:06:33 +0530

    updated

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,43 +1,43 @@
-#!/bin/bash
+#!/bin/bash 
+
+# Validate the user who is running the script is a root user or not.
 
 USER_ID=$(id -u)
 COMPONENT=frontend
 LOGFILE="/tmp/${COMPONENT}.log"
 
-
-if [ $USER_ID -ne 0 ] ; then
-    echo -e "\e[31m script needs to executed by root user or sudo privilage \e[0m" \n \t Example: \n\t\t sudo wrapper.sh frontend
+if [ $USER_ID -ne 0 ] ; then    
+    echo -e "\e[31m Script is expected to executed by the root user or with a sudo privilege \e[0m \n \t Example: \n\t\t sudo bash wrapper.sh frontend"
     exit 1
-
-fi
+fi 
 
 stat() {
-
-    if [ $1 -eq 0 ] ; then
-       echo -e "\e[32m success \e[0m"
-    else  
-       echo -e "\e[31m failure \e[0m"
+    if [ $1 -eq 0 ]; then 
+        echo -e "\e[32m success \e[0m"
+    else 
+        echo -e "\e[31m failure \e[0m"
+        exit 2
     fi
-}   
+}
 
-echo -e "\e[35m configuring ${COMPONENT} \e[0m \n"
+echo -e "\e[35m Configuring ${COMPONENT} ......! \e[0m \n"
 
-echo -n "Installing nginx :"
-yum install nginx -y   &>>   ${LOGFILE}
+echo -n "Installing Nginx :"
+yum install nginx -y     &>>  ${LOGFILE}
 stat $?
 
-echo -n "starting Nginx:"
-systemctl enable nginx  &>>   ${LOGFILE}
-systemctl start nginx   &>>   ${LOGFILE}
+echo -n "Starting Nginx:" 
+systemctl enable nginx   &>>  ${LOGFILE}
+systemctl start nginx    &>>  ${LOGFILE}
 stat $?
 
-echo -n "Downloading ${COMPONENT} component"
-curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
-stat $?
+echo -n "Downloading the ${COMPONENT} component:"
+curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip" 
+stat $? 
 
-echo -n "clean up the ${COMPONENT}:"
-cd /usr/share/nginx/html
-rm -rf *      &>>   ${LOGFILE}
+echo -n "Clean up of ${COMPONENT} : "
+cd /usr/share/nginx/html    
+rm -rf *     &>>  ${LOGFILE}
 stat $?
 
 echo -n "Extracting ${COMPONENT} :"
@@ -49,13 +49,14 @@ mv localhost.conf /etc/nginx/default.d/roboshop.conf
 stat $?
 
 echo -n "Updating the Backend Components in the reverse proxy file:"
-for component in catalogue user cart shiiping payment ; do
+
+for component in catalogue user cart shipping payment ; do 
     sed -i -e "/${component}/s/localhost/${component}.roboshop.internal/" /etc/nginx/default.d/roboshop.conf
-done
+done 
 
 echo -n "Restarting ${COMPONENT}:"
-systemctl daemon-reload     &>>   ${LOGFILE}
-systemctl restart nginx     &>>   ${LOGFILE}
+systemctl daemon-reload     &>>  ${LOGFILE}
+systemctl restart nginx     &>>  ${LOGFILE}
 stat $?
 
 echo -e "\e[35m ${COMPONENT} Installation Is Completed \e[0m \n"
