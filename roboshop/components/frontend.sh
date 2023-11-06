04bfa37caf73f0c03c339546158050e19a7255ee
Lokesh Karasala <lokeshkarasala@Lokeshs-iMac.local>
2023-11-06 14:56:06 +0530

    upda

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -19,7 +19,7 @@ stat() {
     else  
        echo -e "\e[31m failure \e[0m"
        exit 2
-    fi
+    fi 
 }   
 
 echo -e "\e[35m configuring ${COMPONENT} \e[0m \n"
@@ -33,4 +33,32 @@ systemctl enable nginx
 systemctl start nginx
 stat $?
 
+echo -n "Downloading the ${COMPONENT} component:"
+curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip" 
+stat $? 
 
+echo -n "Clean up of ${COMPONENT} : "
+cd /usr/share/nginx/html    
+rm -rf *     &>>  ${LOGFILE}
+stat $?
+
+echo -n "Extracting ${COMPONENT} :"
+unzip /tmp/${COMPONENT}.zip     &>>  ${LOGFILE}
+mv ${COMPONENT}-main/*  .
+mv static/* . 
+rm -rf ${COMPONENT}-main README.md
+mv localhost.conf /etc/nginx/default.d/roboshop.conf
+stat $?
+
+echo -n "Updating the Backend Components in the reverse proxy file:"
+
+for component in catalogue user cart shipping payment ; do 
+    sed -i -e "/${component}/s/localhost/${component}.roboshop.internal/" /etc/nginx/default.d/roboshop.conf
+done 
+
+echo -n "Restarting ${COMPONENT}:"
+systemctl daemon-reload     &>>  ${LOGFILE}
+systemctl restart nginx     &>>  ${LOGFILE}
+stat $?
+
+echo -e "\e[35m ${COMPONENT} Installation Is Completed \e[0m \n"
