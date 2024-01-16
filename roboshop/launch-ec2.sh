c676110381843e191b42ed053cad9a1491e0c3bf
Lokesh Karasala <lokeshkarasala@Lokeshs-iMac.local>
2024-01-16 12:16:22 +0530

    pp

--- roboshop/launch-ec2.sh
+++ roboshop/launch-ec2.sh
@@ -7,12 +7,19 @@
 # 4) Instance you needed
 # 5) DNS record: Hosted zone ID
 
+COMPONENT=$1
+if  [ -z $1 ] ; then
+     echo -e "\e[32m COMPONENT NAME IS NEEDED \e[0m \n \t \t"
+     echo -e "\e[31m Ex Usage \e[0m \n\t\t $ bash launch-ec2.sh shipping"
+     exit1
+fi
+     
 AMI_ID="ami-0f75a13ad2e340a58"
 INSTANCE_TYPE="t3.micro"
 SG_ID="sg-0cc65d7ddd0b64dad"
 
 
 
-aws ec2 run-instances --image-id ${AMI_ID} --count 1 --instance-type ${INSTANCE_TYPE}  --security-group-ids ${SG_ID} --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]
+aws ec2 run-instances --image-id ${AMI_ID} --count 1 --instance-type ${INSTANCE_TYPE}  --security-group-ids ${SG_ID} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"
 
 
