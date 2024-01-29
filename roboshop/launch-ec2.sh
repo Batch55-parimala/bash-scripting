22ce0b1418f119ffc90440ce511edd047702f130
Lokesh Karasala <lokeshkarasala@Lokeshs-iMac.local>
2024-01-29 16:36:45 +0530

    up

--- roboshop/launch-ec2.sh
+++ roboshop/launch-ec2.sh
@@ -19,6 +19,6 @@ INSTANCE_TYPE="t3.micro"
 SG_ID="sg-0cc65d7ddd0b64dad"
 
 
-PRIVATEIP=$(aws ec2 run-instances --image-id "ami-0f75a13ad2e340a58" --instance-type t3.micro  --security-group-ids sg-0cc65d7ddd0b64dad --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=lab}]" | jq '.Instances[].PrivateIpAddress')
+PRIVATEIP=$(aws ec2 run-instances --image-id "ami-0f75a13ad2e340a58" --instance-type t3.micro  --security-group-ids sg-0cc65d7ddd0b64dad --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq '.Instances[].PrivateIpAddress')
 
 echo "Private IP addrees of the $COMPONENT is $PRIVATEIP"
