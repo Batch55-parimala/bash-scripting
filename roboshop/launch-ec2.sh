1754c4c960e30213498acb4cad82d286dba28b74
Lokesh Karasala <lokeshkarasala@Lokeshs-iMac.local>
2024-04-12 01:07:14 +0530

    update

--- roboshop/launch-ec2.sh
+++ roboshop/launch-ec2.sh
@@ -8,18 +8,46 @@
 # 5) DNS record: Hosted zone ID
 
 COMPONENT=$1
+HOSTEDZONE_ID="Z02308013R19EM36500WS"
+INSTANCE_TYPE="t3.micro"
+
 if  [ -z $1 ] ; then
      echo -e "\e[31m COMPONENT NAME IS NEEDED \e[0m \n \t \t"
      echo -e "\e[35m Ex Usage \e[0m \n\t\t $ bash launch-ec2.sh shipping"
      exit1
 fi
      
-AMI_ID="ami-0f75a13ad2e340a58"
-INSTANCE_TYPE="t3.micro"
-SG_ID="sg-0cc65d7ddd0b64dad"
+AMI_ID="$(aws ec2 describe-images --filters "Name=name,Values= DevOps-LabImage-CentOS7" | jq ".Images[].ImageId" | sed -e 's/"//g')"
+SG_ID="$(aws ec2 describe-security-groups --filters Name=group-nam, nValues=B55_Allow all | jq . '.SecurityGroups[].GroupID' sed -e 's/"//g')"
+
+crate_ec2() {
+
+   echo -e "*****Creating \e[35m ${COMPONENT} \e[0m server is in progress ******"
+   PRIVATEIP=$(aws ec2 run-instances --image-id "ami-0f75a13ad2e340a58" --instance-type t3.micro  --security-group-ids sg-0cc65d7ddd0b64dad --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
+   echo -e "Private IP addrees of the $COMPONENT is $PRIVATEIP \n\n"
+   echo -e "Creating DNS record of ${COMPONENT}: "
 
 
-PRIVATEIP=$(aws ec2 run-instances --image-id "ami-0f75a13ad2e340a58" --instance-type t3.micro  --security-group-ids sg-0cc65d7ddd0b64dad --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
+   sed -e "s/$COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${PRIVATEIP}/" route53.json  >/tmp/r53.json
+   aws route53 change-resource-record-sets --hosted-zone-id $HOSTEDZONE_ID --change-batch file:///tmp/r53.json
+   echo -e "Creating DNS record for $COMPONENT has completed \n\n"
 
-echo "Private IP addrees of the $COMPONENT is $PRIVATEIP"
+}
+
+if [ "$1" == "all" ]; then
+
+    for component in mongodb payment cart redis rabbitmq user frontend shipping catalogue mysql; do
+        COMPONENT=$COMPONENT
+        create_ec2
+    done
+
+else    
+   
+     create_ec2
+
+fi     
+
+
+
+
 
