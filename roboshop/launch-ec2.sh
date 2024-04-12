60e0d0cadb843d26e3d4d1bb03055b607de9fab6
Lokesh Karasala <lokeshkarasala@Lokeshs-iMac.local>
2024-04-12 14:13:00 +0530

    update

--- roboshop/launch-ec2.sh
+++ roboshop/launch-ec2.sh
@@ -1,51 +1,46 @@
 #!/bin/bash
 
-#To create a server, what all information is needed?
-# 1) AMI ID
-# 2) Type of instance
-# 3) security group ID
-# 4) Instance you needed
-# 5) DNS record: Hosted zone ID
 
-COMPONENT=$1
-HOSTEDZONE_ID="Z02308013R19EM36500WS"
+
+COMPONENT=$1 
+ENV=$2
+HOSTEDZONEID="Z02308013R19EM36500WS"
 INSTANCE_TYPE="t3.micro"
+ 
+if [ -z $1 ] || [ -z $2 ]  ; then 
+    echo -e "\e[31m COMPONENT NAME IS NEEDED \e[0m \n \t \t"
+    echo -e "\e[35m Ex Usage \e[0m \n\t\t $ bash launch-ec2.sh shipping"
+    exit 1
+fi 
 
-if  [ -z $1 ] ; then
-     echo -e "\e[31m COMPONENT NAME IS NEEDED \e[0m \n \t \t"
-     echo -e "\e[35m Ex Usage \e[0m \n\t\t $ bash launch-ec2.sh shipping"
-     exit1
-fi
-     
-AMI_ID="$(aws ec2 describe-images --filters "Name=name,Values= DevOps-LabImage-CentOS7" | jq ".Images[].ImageId" | sed -e 's/"//g')"
-SG_ID="$(aws ec2 describe-security-groups --filters Name=group-name,Values=B55_Allow all | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')"
+# AMI_ID="ami-0c1d144c8fdd8d690"
+AMI_ID="$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7"| jq ".Images[].ImageId" | sed -e 's/"//g')" 
+SG_ID="$(aws ec2 describe-security-groups  --filters Name=group-name,Values=B55_Allow all | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')"       # b54-allow-all security group id
 
 create_ec2() {
 
-   echo -e "*****Creating \e[35m ${COMPONENT} \e[0m server is in progress ******"
-   PRIVATEIP=$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro  --security-group-ids ${SG_ID} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
-   echo -e "Private IP addrees of the $COMPONENT is $PRIVATEIP \n\n"
-   echo -e "Creating DNS record of ${COMPONENT}: "
+    echo -e "****** Creating \e[35m ${COMPONENT} \e[0m Server Is In Progress ************** "
+    PRIVATEIP=$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type ${INSTANCE_TYPE}  --security-group-ids ${SG_ID} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}-${ENV}}]" | jq '.Instances[].PrivateIpAddress'| sed -e 's/"//g') 
 
+    echo -e "Private IP Address of the $COMPONENT-${ENV} is $PRIVATEIP \n\n"
+    echo -e "Creating DNS Record of ${COMPONENT}: "
 
-   sed -e "s/$COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${PRIVATEIP}/" route53.json  >/tmp/r53.json
-   aws route53 change-resource-record-sets --hosted-zone-id $HOSTEDZONE_ID --change-batch file:///tmp/r53.json
-   echo -e "Creating DNS record for $COMPONENT has completed \n\n"
+    sed -e "s/COMPONENT/${COMPONENT}-${ENV}/"  -e "s/IPADDRESS/${PRIVATEIP}/" route53.json  > /tmp/r53.json 
 
+    aws route53 change-resource-record-sets --hosted-zone-id $HOSTEDZONEID --change-batch file:///tmp/r53.json
+    echo -e "\e[36m **** Creating DNS Record for the $COMPONENT has completed **** \e[0m \n\n"
 }
 
-if [ "$1" == "all" ]; then
+if [ "$1" == "all" ]; then 
 
-    for component in mongodb payment cart redis rabbitmq user frontend shipping catalogue mysql; do
-        COMPONENT=$component
+    for component in mongodb catalogue cart user shipping frontend payment mysql redis rabbitmg; do 
+        COMPONENT=$component 
         create_ec2
-    done
+    done 
 
-else    
-   
-     create_ec2
-
-fi       
+else 
+        create_ec2 
+fi     
 
 
 
